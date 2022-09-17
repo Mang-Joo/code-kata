f086883cd53d28487088adce0bbd31a8c03a1541
KimWanJoo <wjwan0915@gmail.com>
2022-09-17 19:34:07 +0900

    :white_check_mark: Computer Test
    컴퓨터 클래스 테스트 작성

---
+++ number-baseball-third/src/test/java/io/github/mangjoo/domain/Player/ComputerTest.java
@@ -0,0 +1,50 @@
+package io.github.mangjoo.domain.Player;
+
+import io.github.mangjoo.domain.Balls;
+import io.github.mangjoo.domain.MakeRandomBall;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ComputerTest {
+    @Test
+    void 문자열_생성_테스트() {
+        //given
+        String strBalls = "123";
+
+        //then
+        assertDoesNotThrow(() -> Player.Computer(strBalls));
+    }
+
+    @Test
+    void 배열_생성_테스트() {
+        //given
+        int[] arrayBalls = new int[]{1, 2, 3};
+
+        //then
+        assertDoesNotThrow(() -> Player.Computer(arrayBalls));
+    }
+
+    @Test
+    void 위임_생성_테스트() {
+        //given
+        MakeRandomBall makeRandomBall = new MakeRandomBall();
+
+        //then
+        assertDoesNotThrow(() -> Player.Computer(makeRandomBall));
+    }
+
+    @Test
+    void 공_가져오기_테스트() {
+        //given
+        String inputBall = "123";
+
+        //when
+        Player computer = Player.Computer(inputBall);
+        Balls balls = new Balls(inputBall);
+
+        //then
+        assertEquals(computer.balls(), balls);
+    }
+}
